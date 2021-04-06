3e455b403856baed5c44cec615608cd83e5a8417
moses877891 <mosesantonymas@gmail.com>
2021-04-06 11:55:50 +0530

    Completing the project

--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -3,8 +3,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import CheckoutItem from '../../components/checkout-item/checkout-item.component'
-import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import "./chechout.scss";
 
@@ -36,6 +37,12 @@ const CheckoutPage = ({ cartItems, total }) => (
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payments*
+            <br />
+            4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
